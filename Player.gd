00913bcdcf98f754572419f339caf89d756f507a
Drew Wei <drewngui@gmail.com>
2023-01-05 14:56:22 -0800

    Finished up to Lesson 10

--- Player.gd
+++ Player.gd
@@ -1,61 +1,156 @@
 extends KinematicBody2D
 class_name Player
 
-export(int) var JUMP_FORCE = -130
-export(int) var JUMP_RELEASE_FORCE = -70
-export(int) var MAX_SPEED = 90
-export(int) var ACCELERATION = 10
-export(int) var FRICTION = 10
-export(int) var GRAVITY = 4
-export(int) var ADDITIONAL_FALL_GRAVITY = 4
+enum {
+    MOVE,
+    CLIMB
+}
 
+
+export(Resource) var moveData = preload("res://FastPlayerMovementData.tres") as PlayerMovementData
 
 var velocity = Vector2.ZERO
+var state = MOVE
+var double_jump = 1
+var buffered_jump = false
+var coyote_jump = false
 
-onready var animatedSprite = $AnimatedSprite
+onready var animatedSprite: = $AnimatedSprite
+onready var ladderCheck: = $LadderCheck
+onready var jumpBufferTimer: = $JumpBufferTimer
+onready var coyoteJumpTimer: = $CoyoteJumpTimer
 
 func _ready():
     animatedSprite.frames = load("res://PlayerGreenSkin.tres")
+    
+#func powerup():
+#    moveData = load("res://FastPlayerMovementData.tres")
 
 func _physics_process(delta):
-    apply_gravity()
     var input = Vector2.ZERO
-    input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+#    input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+    input.x = Input.get_axis("ui_left", "ui_right") # cleaner shortcut of above
+    input.y = Input.get_axis("ui_up", "ui_down") # cleaner shortcut of above
     
-    if input.x == 0:
+    match state:
+        MOVE: move_state(input)
+        CLIMB: climb_state(input)
+    
+        
+func move_state(input):
+    if is_on_ladder() and Input.is_action_pressed("ui_up"):
+        state = CLIMB
+    
+    apply_gravity()
+    
+    if not horizontal_move(input):
         apply_friction()
         animatedSprite.animation = "Idle"
     else:
         apply_acceleration(input.x)
         animatedSprite.animation = "Run"
-        
         animatedSprite.flip_h = input.x > 0
     
     if is_on_floor():
-        if Input.is_action_pressed("ui_up"):
-            velocity.y = JUMP_FORCE
+        reset_double_jump()
     else:
         animatedSprite.animation = "Jump"
-        if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
-            velocity.y = JUMP_RELEASE_FORCE
-            
-        if velocity.y > 0:
-            velocity.y += ADDITIONAL_FALL_GRAVITY
+    
+    # Coyote jump takes priority over double jump. Else might be a bug?
+    if can_jump():
+        input_jump()
+    else:
+        # input jump release
+        input_jump_release()
+        
+        # Double jump logic.
+        input_double_jump()
+        
+        # Jump Buffer logic. Player just pressed right before hitting the ground -- buffer a jump
+        buffer_jump()
+        
+        # fast fall
+        fast_fall()
             
     
     var was_in_air = not is_on_floor()
+    var was_on_floor = is_on_floor()
+    
     velocity = move_and_slide(velocity, Vector2.UP)
+    
     var just_landed = is_on_floor() and was_in_air
     if just_landed:
         animatedSprite.animation = "Run"
         animatedSprite.frame = 1
+    
+    # Coyote Jump
+    var just_left_ground = not is_on_floor() and was_on_floor
+    if just_left_ground and velocity.y >= 0:
+        coyote_jump = true
+        coyoteJumpTimer.start()
+    
+func climb_state(input):
+    if not is_on_ladder(): state = MOVE
+    if input.length() != 0:
+        animatedSprite.animation = "Run"
+    else:
+        animatedSprite.animation = "Idle"
+    velocity = input * moveData.CLIMB_SPEED
+    velocity = move_and_slide(velocity, Vector2.UP)
+    
+func input_jump_release():
+    if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+        velocity.y = moveData.JUMP_RELEASE_FORCE
+
+func input_double_jump():
+    if Input.is_action_just_pressed("ui_up") and double_jump > 0:
+        velocity.y = moveData.JUMP_FORCE
+        double_jump -= 1
+    
+func buffer_jump():
+    if Input.is_action_just_pressed("ui_up"):
+        buffered_jump = true
+        jumpBufferTimer.start()
+
+func fast_fall():
+    if velocity.y > 0:
+        velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
+        
+func can_jump():
+    return is_on_floor() or coyote_jump
+    
+func horizontal_move(input):
+    return input.x != 0
+
+func reset_double_jump():
+    # Reset double jump
+    double_jump = moveData.DOUBLE_JUMP_COUNT # Reset double_jump
+
+func input_jump():
+    if Input.is_action_just_pressed("ui_up") or buffered_jump:
+        velocity.y = moveData.JUMP_FORCE
+        buffered_jump = false
+
+func is_on_ladder():
+    if not ladderCheck.is_colliding(): return false
+    var collider = ladderCheck.get_collider()
+    if not collider is Ladder: return false
+    return true
 
 func apply_gravity():
-    velocity.y += GRAVITY
+    velocity.y += moveData.GRAVITY
     velocity.y = min(velocity.y, 500)
 
 func apply_friction():
-    velocity.x = move_toward(velocity.x, 0, FRICTION)
+    velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
     
 func apply_acceleration(amount):
-    velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+    velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
+
+
+func _on_JumpBufferTimer_timeout():
+    buffered_jump = false
+
+
+func _on_CoyoteJumpTimer_timeout():
+    coyote_jump = false
