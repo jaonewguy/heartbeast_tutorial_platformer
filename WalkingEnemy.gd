0fd8ba39470ec250e5a1c0c834cbace017a3bc74
Drew Wei <drewngui@gmail.com>
2023-01-05 00:33:09 -0800

    Completing lesson 6 and correcting spikes

--- WalkingEnemy.gd
+++ WalkingEnemy.gd
@@ -5,6 +5,7 @@ var velocity = Vector2.ZERO
 
 onready var ledgeCheckRight: = $LedgeCheckRight
 onready var ledgeCheckLeft: = $LedgeCheckLeft
+onready var sprite: = $AnimatedSprite
 
 func _physics_process(delta):
     var found_wall = is_on_wall()
@@ -12,6 +13,8 @@ func _physics_process(delta):
     
     if found_wall or found_ledge:
         direction *= -1
+        
+        sprite.flip_h = direction.x > 0
     
     velocity = direction * 25
     move_and_slide(velocity, Vector2.UP)
